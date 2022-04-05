83eb50d83efe370500eccdf0fb0e92c3ee737307
pspanoudakis <pavlos.spanoudakis@gmail.com>
2022-04-05 10:27:11 +0300

    Attempt to submit rules solution

--- rules/run.py
+++ rules/run.py
@@ -4,19 +4,40 @@ from handler_x1 import handle_x1
 Flag = True
 
 if __name__ == '__main__':
-    for file_name in ['../datasets/X1.csv']:
+    couples = []
+    for file_name in ['./X1.csv', './X2.csv']:
         data = pd.read_csv(file_name)
         if 'name' not in data.columns:
-            output = handle_x1(data)
-            
-        if Flag:
-            output.to_csv("../datasets/output.csv", sep=',', encoding='utf-8', index=False)
-            Flag = False
+            couples.extend(handle_x1(data))
         else:
-            output.to_csv(
-                "output.csv",
-                mode='a',
-                sep=',',
-                encoding='utf-8',
-                index=False,
-                header=None)
+            if Flag:
+                couples.extend([(0,0)]*2)
+            else:
+                couples.extend([(0,0)]*2000000)
+
+    output = pd.DataFrame(
+                couples,
+                columns=[
+                    'lid',
+                    'rid',
+                    'label'])
+    output.drop(columns=['label'], inplace=True)
+    output.to_csv(
+            "./output.csv",
+            mode='a',
+            sep=',',
+            encoding='utf-8',
+            index=False,
+            header=None)
+
+    """ if Flag:
+        output.to_csv("../datasets/output.csv", sep=',', encoding='utf-8', index=False)
+        Flag = False
+    else:
+        output.to_csv(
+            "output.csv",
+            mode='a',
+            sep=',',
+            encoding='utf-8',
+            index=False,
+            header=None) """

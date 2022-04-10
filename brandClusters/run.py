b8d9cddee51c765ce58e0a09a474d879e6bc9ea4
pspanoudakis <pavlos.spanoudakis@gmail.com>
2022-04-10 14:43:02 +0300

    Sorting titles before clustering

--- brandClusters/run.py
+++ brandClusters/run.py
@@ -1,15 +1,21 @@
 from typing import Dict, List, Tuple
 from collections import defaultdict
-from difflib import SequenceMatcher
 import string
+
+USE_C_SEQ_MATHCER = False
+
+if USE_C_SEQ_MATHCER:
+    from cdifflib import CSequenceMatcher
+else:
+    from difflib import SequenceMatcher
 
 import pandas as pd
 
-SUBMISSION_MODE = True
+SUBMISSION_MODE = False
 
-SEQ_MATCH_THRESHOLD = 0.9
+SEQ_MATCH_THRESHOLD = 0.925
 
-def saveAndExit(pairs):
+def saveAndExit(pairs: List[ Tuple[int, int] ]):
     file = "output.csv"
     if SUBMISSION_MODE:
         
@@ -26,7 +32,8 @@ def saveAndExit(pairs):
         
         pairs.extend([(0,0)]*expected_cand_size_X2)
     else:
-        pairs.extend([(0,0)]*2)
+        pass
+        #pairs.extend([(0,0)]*2)
     
     out = pd.DataFrame(
                 pairs,
@@ -35,12 +42,13 @@ def saveAndExit(pairs):
                     'right_instance_id'])
 
     out.to_csv(file, index=False)
+    exit()
 
 def cleanInstance(raw: str):
     raw_low = raw.lower()
     no_punct = raw_low.translate(str.maketrans({ord(c): ' ' for c in string.punctuation}))
     words = no_punct.split()
-    #words.sort()
+    words.sort()
 
     return words, ' '.join(words)
 
@@ -72,39 +80,43 @@ familyNames = {
     'yoga'
 }
 
-pairs: List[Tuple[int, int]] = []
-
-brandClusters: Dict[str, List[ Tuple[int, str] ]] = defaultdict(list)
+UKNOWN_FAMILY = 'unknown'
 
 if __name__ == '__main__':
+    pairs: List[Tuple[int, int]] = []
+    brandClusters: Dict[str, List[ Tuple[int, str, List[str]] ]] = defaultdict(list)
+
     pd.read_csv("X2.csv")
     X = pd.read_csv("X1.csv")
 
     for i, row in X.iterrows():
-        words, cleaned = cleanInstance(row['title'])
+        sortedWords, cleanedTitle = cleanInstance(row['title'])
 
-        for word in words:
+        for word in sortedWords:
             if word in brandNames:
-                brandClusters[word].append((row['id'], cleaned))
+                brandClusters[word].append((row['id'], cleanedTitle, sortedWords))
                 break
         
     for brand in brandClusters:
         familyClusters: Dict[str, List[ Tuple[int, str] ]] = defaultdict(list)
 
-        for instanceId, title in brandClusters[brand]:
-            words = title.split()
+        for instanceId, title, words in brandClusters[brand]:
+            unkownFamily = True
             for word in words:
                 if word in familyNames:
                     familyClusters[word].append((instanceId, title))
+                    unkownFamily = False
                     break
+            
+            if unkownFamily:
+                familyClusters[UKNOWN_FAMILY].append((instanceId, title))
+        
+        # Maybe save some memory?
+        brandClusters[brand] = None
         
         # Family subcluster
         for family in familyClusters:
-            for i, (instanceId, title) in enumerate(familyClusters[family]):
-                sortedTitleList = title.split()
-                sortedTitleList.sort()
-                sortedTitle = ' '.join(sortedTitleList)
-
+            for i, (instanceId, sortedTitle) in enumerate(familyClusters[family]):
                 familyClusters[family][i] = (instanceId, sortedTitle)
                 
             for i in range(len(familyClusters[family])):
@@ -116,7 +128,11 @@ if __name__ == '__main__':
                     if seqMatch.ratio() >= SEQ_MATCH_THRESHOLD:
                         if i_id < j_id:
                             pairs.append( (i_id, j_id) )
+                            if len(pairs) == 1000000:
+                                saveAndExit(pairs)
                         else:
                             pairs.append( (j_id, i_id) )
+                            if len(pairs) == 1000000:
+                                saveAndExit(pairs)
         
     saveAndExit(pairs)

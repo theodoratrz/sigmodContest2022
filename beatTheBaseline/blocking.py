b1a21947969542178258d2054a8b7c3d668732c4
theodoratrz <theodora.trz@gmail.com>
2022-04-18 00:09:47 +0300

    Merge branch 'baseline-mods-exp' of https://github.com/theodoratrz/sigmodContest2022 into baseline-mods-exp
    Co-authored-by: Pavlos Spanoudakis <pspanoudakis@users.noreply.github.com>

--- beatTheBaseline/blocking.py
+++ beatTheBaseline/blocking.py
@@ -8,8 +8,113 @@ import pandas as pd
 from utils import *
 
 
-def x2_blocking(csv_reader, save_scores=False) -> List[Tuple[int, int]]:
-    return []
+def x2_blocking(csv_reader, id_col: str, title_col: str, save_scores=False) -> List[Tuple[int, int]]:
+    """
+    This function performs blocking on X1 dataset.
+    :param `X`: dataframe
+    :return: candidate set of tuple pairs
+    """
+
+    sameSequencePatterns: Dict[str, List[ Tuple[int, str] ]] = defaultdict(list)
+    modelPatterns: Dict[str, List[ Tuple[int, str] ]] = defaultdict(list)
+
+    for i, row in enumerate(csv_reader):
+        id = int(row[id_col])
+        rawTitle = str(row[title_col]).lower()
+
+        noTrash = re.sub(trashPattern, '', rawTitle)
+        cleanedTitle = noTrash.translate(str.maketrans({ord(c): ' ' for c in customPunctuation}))
+        cleanedTitle = re.sub(' +', ' ', cleanedTitle).strip()
+
+        clean_words = cleanedTitle.split()
+        clean_words.sort()
+        unique_words = {word: None for word in clean_words}.keys()
+        sortedTitle = ' '.join(unique_words)
+
+        #instance = (id, cleanedTitle)
+        instance = (id, rawTitle)
+
+        sameSequencePatterns[sortedTitle].append(instance)
+
+        #brand = brandPattern.search(sortedTitle)
+        #if not brand:
+        #    brand = 'no_brand'
+        #else:
+        #    brand = brand.group()
+        brands = []
+        for brand in brandPatterns:
+            match = re.search(brand, sortedTitle)
+            if match:
+                brands.append(match.group())
+        brand = ' & '.join(brands) if len(brands) else 'no_brand'
+        
+        model = modelPattern.search(cleanedTitle)
+        if not model:
+            model = 'no_model'
+        else:
+            model = model.group()
+
+        cpu = cpuModelPattern.search(cleanedTitle)
+        if not cpu:
+            #cpu = cpuBrandPattern.search(cleanedTitle)
+            #cpu = cpu.group() if cpu else 'no_cpu'
+            cpu = 'no_cpu'
+        else:
+            cpu = cpu.group()
+
+        if model != 'no_model' and cpu != 'no_cpu':
+            pattern = " || ".join((brand, model, cpu))
+            modelPatterns[pattern].append(instance)
+
+        #possibleModels = re.findall("\w+\s\w+\d+", cleanedTitle)  # look for patterns like "thinkpad x1"
+        #if len(possibleModels) > 0:
+        #    possibleModels = list(sorted(possibleModels))
+        #    possibleModels = [str(m).lower() for m in possibleModels]
+        #    modelPatternToId[" ".join(possibleModels)].append(i)
+
+    # add id pairs that share the same pattern to candidate set
+    candidate_pairs_1 = []
+    for pattern in sameSequencePatterns:
+        instances = sameSequencePatterns[pattern]
+        for i in range(len(instances)):
+            for j in range(i + 1, len(instances)):
+                candidate_pairs_1.append((instances[i], instances[j])) #
+    # add pairs that share the same pattern to candidate set
+    candidate_pairs_2 = []
+    for pattern in modelPatterns:
+        instances = modelPatterns[pattern]
+        if len(instances)<500: #skip patterns that are too common
+            for i in range(len(instances)):
+                for j in range(i + 1, len(instances)):
+                    candidate_pairs_2.append((instances[i], instances[j]))
+
+    # remove duplicate pairs and take union
+    candidate_pairs_1 = set(candidate_pairs_1)
+    candidate_pairs_2 = set(candidate_pairs_2)
+    candidate_pairs = list(candidate_pairs_2.union(candidate_pairs_1))
+
+    # sort candidate pairs by jaccard similarity.
+    # In case we have more than 1000000 pairs (or 2000000 pairs for the second dataset),
+    # sort the candidate pairs to put more similar pairs first,
+    # so that when we keep only the first 1000000 pairs we are keeping the most likely pairs
+    jaccard_similarities = []
+    candidate_pairs_real_ids: List[Tuple[int, int]] = []
+    for pair in candidate_pairs:
+        (id1, name1), (id2, name2) = pair
+
+        if id1 < id2: # NOTE: This is to make sure in the final output.csv, for a pair id1 and id2 (assume id1<id2), we only include (id1,id2) but not (id2, id1)
+            candidate_pairs_real_ids.append((id1, id2))
+        else:
+            candidate_pairs_real_ids.append((id2, id1))
+
+        # compute jaccard similarity
+        jaccard_similarities.append(getSimilarityScore(name1, name2))
+
+    if save_scores:
+        candidate_pairs_real_ids = [(pair[0], pair[1], score) for pair, score in sorted(zip(candidate_pairs_real_ids, jaccard_similarities), key=lambda t: t[1], reverse=True)]
+    else:
+        candidate_pairs_real_ids = [x for _, x in sorted(zip(jaccard_similarities, candidate_pairs_real_ids), reverse=True)]
+    return candidate_pairs_real_ids
 
 def x1_blocking(csv_reader, id_col: str, title_col: str, save_scores=False) -> List[Tuple[int, int]]:
     """
--- beatTheBaseline/utils.py
+++ beatTheBaseline/utils.py
@@ -14,6 +14,7 @@ trash = [
     r"amazon",
     r"alibaba",
     r"\d+ ?[t][b]",
+    #r"\d+ ?[g][b]",
     r"hdd",
     r"ssd",
     r"vology",
@@ -86,6 +87,7 @@ modelPattern = re.compile('|'.join(models))
 
 cpus = [
     # intel
+    #r'(intel )?(core )?i[357]( [0-9]{3,4}[q]?[mu])?',
     r'(intel )?(core )?i3( [0-9]{3,4}[q]?[mu])?',
     r'(intel )?(core )?i5( [0-9]{3,4}[q]?[mu])?',
     r'(intel )?(core )?i7( [0-9]{3,4}[q]?[mu])?',
@@ -134,3 +136,123 @@ def getSimilarityScore(a: str, b: str) -> float:
             weighted_sum += ALPHANUMERIC_WEIGHT
 
     return weighted_sum/max(len(a_words), len(b_words))
+
+class NamespaceX2:
+    punctuation = re.sub(r'[.-]', '', string.punctuation)
+
+    trash = [
+        r"[a-z]+(.com|.ca|.es|.de|.fr)",
+        r"com:"
+        r"amazon",
+        r"clé",
+        r"tesco",
+        r"mediamarkt",
+        r"alibaba",
+        r"vology",
+        r"downgrade",
+        r"brand new",
+        r"china",
+        r"buy",
+        r"famous",
+        r"australia",
+        r"(online in )?egypt",
+        r"online",
+        r"accessories",
+        r"wholeshale"
+        r"e[b]ay",
+        r"best",
+        r"and",
+        r"kids",
+        r"bit",
+        r"win",
+        r"com",
+        r"general",
+        r"cheap",
+        r"with",
+        r"great",
+        r"product",
+        r"quality",
+        r"flash",
+        r"drive",
+        r"memory",
+        r"tarjeta",
+        r"adaptateur"
+        r"m[eé]mo(ria|ire)",
+        r"class[e]? "
+        r"carte",
+        r"card",
+    ]
+    trashPattern = re.compile('|'.join(trash))
+
+    brandPatterns = [
+        r"sony",
+        r"toshiba",
+        r"kingston",
+        r"sandisk",
+        r"intenso",
+        r"lexar",
+        r"transcend",
+        r"samsung",
+        r"pny"
+    ]
+    brandPattern = re.compile('|'.join(brandPatterns))
+
+    models = [
+        r"galaxy [ajs][0-9]{1,2}"
+        r"galaxy note[ ]?[0-9]{1,2}"
+        r"ljd[\w]+",
+        r"lsd[\w]+",
+        r"thn[\w]+",
+        r"micro[ ]?sd[xh]c",
+        r"micro[ ]?sd",
+        r"sd[xh]c",
+        r"usb [23].[01]",
+        r"usb [23]",
+    ]
+    modelPattern = re.compile('|'.join(models))
+
+
+    def unifyCapacities(s: str):
+        separatedCapacity = r'(?P<size>[0-9]{1,3}) (?P<unit>[gt]b)'
+
+        return re.sub(separatedCapacity, r'\g<size>\g<unit>', s)
+    
+    capacityPattern = r'[0-9]{1,3}[gt]b'
+
+    numberPattern = r'[0-9]+(\.[0-9]+)?'
+    NUMBER_WEIGHT = 0.1
+    wordPattern = r'(?=\D)[\w]+'
+    WORD_WEIGHT = 0.2
+    BRAND_WEIGHT = 0.6
+    CAPACITY_WEIGHT = 0.4
+    ALPHANUMERIC_WEIGHT = 1
+
+    def getSimilarityScore(a: str, b: str) -> float:
+        a_words = set(a.split())
+        b_words = set(b.split())
+        if len(a_words) == 0 or len(b_words) == 0:
+            return 0.0
+        
+        common = a_words.intersection(b_words)
+
+        weighted_sum = 0
+        for word in common:
+            match = re.fullmatch(NamespaceX2.wordPattern, word)
+            if match:
+                # Character-only word
+                if NamespaceX2.brandPattern.fullmatch(match.group()):
+                    # Brand
+                    weighted_sum += NamespaceX2.BRAND_WEIGHT
+                else:
+                    weighted_sum += NamespaceX2.WORD_WEIGHT
+            elif re.fullmatch(NamespaceX2.numberPattern, word):
+                # Number
+                weighted_sum += NamespaceX2.NUMBER_WEIGHT
+            else:
+                # Alphanumeric string
+                if re.fullmatch(NamespaceX2.capacityPattern, word):
+                    weighted_sum += NamespaceX2.CAPACITY_WEIGHT
+                else:
+                    weighted_sum += NamespaceX2.ALPHANUMERIC_WEIGHT
+
+        return weighted_sum/max(len(a_words), len(b_words))

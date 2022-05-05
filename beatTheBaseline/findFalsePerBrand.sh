f5489ad8e1abc9681b4392890f79902ef457d877
pspanoudakis <pavlos.spanoudakis@gmail.com>
2022-05-05 12:41:16 +0300

    Compatibility with csv headers

--- beatTheBaseline/findFalsePerBrand.sh
+++ beatTheBaseline/findFalsePerBrand.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
+#read -r columnHeaders < false.csv
 for brand in "sony" "toshiba" "kingston" "sandisk" "intenso" "lexar" "transcend" "samsung" "pny"; do
+    #printf "$columnHeaders\n" > "./false/false_$brand.csv"
+    #cat false.csv | grep -i $brand >> "./false/false_$brand.csv"
     cat false.csv | grep -i $brand > "./false/false_$brand.csv"
     lines=$(wc -l < "./false/false_$brand.csv")
     printf "$brand: $lines\n"
--- beatTheBaseline/findMissedPerBrand.sh
+++ beatTheBaseline/findMissedPerBrand.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
+#read -r columnHeaders < missed.csv
 for brand in "sony" "toshiba" "kingston" "sandisk" "intenso" "lexar" "transcend" "samsung" "pny"; do
+    #printf "$columnHeaders\n" > "./missed/missed_$brand.csv"
+    #cat missed.csv | grep -i $brand >> "./missed/missed_$brand.csv"
     cat missed.csv | grep -i $brand > "./missed/missed_$brand.csv"
     lines=$(wc -l < "./missed/missed_$brand.csv")
     printf "$brand: $lines\n"
